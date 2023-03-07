4098823825f8f219803bd7ae8be0af3517342e93
BranBayou <berhan.baye@gmail.com>
2023-03-07 08:27:27 +0300

    Add mobile menu lists link to navigate the page

--- scripts.js
+++ scripts.js
@@ -1,20 +1,33 @@
 const humMenuBtn = document.querySelector('.humburger-menu-btn');
 const popup = document.querySelector('.mobile-menu');
 const header = document.querySelector('header');
-const closeBtn = document.querySelector('close-btn');
+const closeBtn = document.querySelector('.close-btn');
+const option1 = document.getElementById('menu-port');
+const option2 = document.getElementById('menu-about');
+const option3 = document.getElementById('menu-contact');
 
-humMenuBtn.addEventListener('click', e => {
+humMenuBtn.addEventListener('click', (e) => {
   e.preventDefault();
   popup.style.display = 'block';
   header.style.display = 'none';
 });
 
-closeBtn.addEventListener('click', e => {
+closeBtn.addEventListener('click', () => {
   popup.style.display = 'none';
   header.style.display = 'flex';
 });
 
-popup.addEventListener('click', e => {
+option1.addEventListener('click', () => {
   popup.style.display = 'none';
-  header.style.display = 'flex';
+  header.style.display = 'block';
+});
+
+option2.addEventListener('click', () => {
+  popup.style.display = 'none';
+  header.style.display = 'block';
+});
+
+option3.addEventListener('click', () => {
+  popup.style.display = 'none';
+  header.style.display = 'block';
 });
