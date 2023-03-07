41203a714727c352a4c13e299012b5698ba8c457
BranBayou <berhan.baye@gmail.com>
2023-03-07 16:37:19 +0300

    Add more css and html miner changes

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ const humMenuBtn = document.querySelector('.humburger-menu-btn');
 const popup = document.querySelector('.mobile-menu');
 const header = document.querySelector('header');
 const closeBtn = document.querySelector('.close-btn');
-const option1 = document.getElementById('');
-const option2 = document.getElementById('');
-const option3 = document.getElementById('');
+const option1 = document.getElementById('menu-port');
+const option2 = document.getElementById('menu-about');
+const option3 = document.getElementById('menu-contact');
 
 humMenuBtn.addEventListener('click', (e) => {
   e.preventDefault();
