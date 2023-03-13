1d184b4b92b974c1d599d61e4721510668a10282
BranBayou <berhan.baye@gmail.com>
2023-03-13 15:29:35 +0300

    Add some changes on script.js file

--- scripts.js
+++ scripts.js
@@ -6,28 +6,4 @@ const option1 = document.getElementById('menu-port');
 const option2 = document.getElementById('menu-about');
 const option3 = document.getElementById('menu-contact');
 
-humMenuBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  popup.style.display = 'block';
-  header.style.display = 'none';
-});
-
-closeBtn.addEventListener('click', () => {
-  popup.style.display = 'none';
-  header.style.display = 'flex';
-});
-
-option1.addEventListener('click', () => {
-  popup.style.display = 'none';
-  header.style.display = 'block';
-});
-
-option2.addEventListener('click', () => {
-  popup.style.display = 'none';
-  header.style.display = 'block';
-});
-
-option3.addEventListener('click', () => {
-  popup.style.display = 'none';
-  header.style.display = 'block';
-});
+/* Hamburger menu code ends here*/
